55a517dab27ea9a8e05fe135b379af6ad0a4aa0b
danielwiehl <daniel.wiehl@yahoo.com>
2021-08-04 14:22:24 +0200

    feat(testapp): add toggle to follow message tail

--- apps/solace-message-client-testing-app/src/app/subscriber/subscriber.component.ts
+++ apps/solace-message-client-testing-app/src/app/subscriber/subscriber.component.ts
@@ -1,45 +1,53 @@
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ChangeDetectorRef, Component, OnDestroy, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Message, MessageDumpFlag, MessageEnvelope, MessageType, SolaceMessageClient } from 'solace-message-client';
-import { Subscription } from 'rxjs';
-import { finalize } from 'rxjs/operators';
+import { Subject, Subscription } from 'rxjs';
+import { filter, finalize, takeUntil } from 'rxjs/operators';
 import { SciViewportComponent } from '@scion/toolkit/viewport';
 
 export const TOPIC = 'topic';
+export const SUBSCRIPTION = 'subscription';
+export const FOLLOW_TAIL = 'followTail';
 
 @Component({
   selector: 'app-subscriber',
   templateUrl: './subscriber.component.html',
   styleUrls: ['./subscriber.component.scss'],
 })
-export class SubscriberComponent {
+export class SubscriberComponent implements OnDestroy {
 
   public readonly TOPIC = TOPIC;
+  public readonly SUBSCRIPTION = SUBSCRIPTION;
+  public readonly FOLLOW_TAIL = FOLLOW_TAIL;
 
   public form: FormGroup;
   public subscribeError: string;
   public messageListItems: MessageListItem[] = [];
 
   private _subscription: Subscription;
+  private _destroy$ = new Subject<void>();
 
   @ViewChild(SciViewportComponent)
   private _viewport: SciViewportComponent;
 
-  constructor(formBuilder: FormBuilder,
-              public solaceMessageClient: SolaceMessageClient,
+  constructor(public solaceMessageClient: SolaceMessageClient,
               private _cd: ChangeDetectorRef) {
     this.form = new FormGroup({
-      [TOPIC]: formBuilder.control('', Validators.required),
+      [SUBSCRIPTION]: new FormGroup({
+        [TOPIC]: new FormControl('', Validators.required),
+      }),
+      [FOLLOW_TAIL]: new FormControl(true),
     });
+    this.installFollowTailListener();
   }
 
   public onSubscribe(): void {
-    this.form.disable();
+    this.form.get(SUBSCRIPTION).disable();
     this.subscribeError = null;
 
     try {
-      this._subscription = this.solaceMessageClient.observe$(this.form.get(TOPIC).value)
-        .pipe(finalize(() => this.form.enable()))
+      this._subscription = this.solaceMessageClient.observe$(this.form.get([SUBSCRIPTION, TOPIC]).value)
+        .pipe(finalize(() => this.form.get(SUBSCRIPTION).enable()))
         .subscribe(
           (envelope: MessageEnvelope) => {
             const message: Message = envelope.message;
@@ -52,15 +60,16 @@ export class SubscriberComponent {
               namedWildcardSegments: Array.from(envelope.params.entries()).reduce((params, [key, value]) => params.concat(`${key}->${value}`), []).join(', '),
             });
 
-            // follow tail
-            this._cd.detectChanges();
-            this._viewport.scrollTop = this._viewport.scrollHeight;
+            if (this.form.get(FOLLOW_TAIL).value) {
+              this._cd.detectChanges();
+              this.scrollToEnd();
+            }
           },
           error => this.subscribeError = error.toString(),
         );
     }
     catch (error) {
-      this.form.enable();
+      this.form.get(SUBSCRIPTION).enable();
       this.subscribeError = error;
     }
   }
@@ -80,6 +89,25 @@ export class SubscriberComponent {
 
   public get isSubscribed(): boolean {
     return this._subscription && !this._subscription.closed;
+  }
+
+  private scrollToEnd(): void {
+    this._viewport.scrollTop = this._viewport.scrollHeight;
+  }
+
+  private installFollowTailListener(): void {
+    this.form.get(FOLLOW_TAIL).valueChanges
+      .pipe(
+        filter(Boolean),
+        takeUntil(this._destroy$),
+      )
+      .subscribe(() => {
+        this.scrollToEnd();
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this._destroy$.next();
   }
 }
 
