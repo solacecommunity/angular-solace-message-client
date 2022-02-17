9b6311cfc35ab78374d7489210c45c5a7bf97152
danielwiehl <daniel.wiehl@yahoo.com>
2022-02-17 16:33:09 +0100

    ci(solace-message-client): fix end-to-end test setup to start the application using `http-server`
    In the GitHub Actions workflow, we build library and "TryMe" application as one of the first steps. When running the end-to-end tests, we start the application using `http-server` in order to speed up test execution. Since the "TryMe" application has a context-path set (required for deployment to GitHub Pages), we need to place the application in that context path as well, otherwise resources would not be found.

--- projects/solace-message-client.e2e/src/solace-message-client.e2e-spec.ts
+++ projects/solace-message-client.e2e/src/solace-message-client.e2e-spec.ts
@@ -1,6 +1,9 @@
+import {browser} from 'protractor';
+
 describe('Solace Message Client', () => {
 
-  it('should always be true', async () => {
-    expect(true).toBe(true);
+  it('should have a working end-to-end test setup', async () => {
+    await browser.get('/angular-solace-message-client/tryme');
+    await expect(await browser.getTitle()).toEqual('Angular Solace Message Client');
   });
 });
