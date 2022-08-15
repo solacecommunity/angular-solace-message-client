a0bb4f41dcdc54bd6b49a3811a7b924ad867d72d
danielwiehl <daniel.wiehl@yahoo.com>
2022-08-15 08:50:37 +0200

    ci(solace-message-client): remove e2e Protractor tests

--- projects/solace-message-client.e2e/src/solace-message-client.e2e-spec.ts
+++
@@ -1,9 +0,0 @@
-import {browser} from 'protractor';
-
-describe('Solace Message Client', () => {
-
-  it('should have a working end-to-end test setup', async () => {
-    await browser.get('/angular-solace-message-client/tryme');
-    await expect(await browser.getTitle()).toEqual('Angular Solace Message Client');
-  });
-});
