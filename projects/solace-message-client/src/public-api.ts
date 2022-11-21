6469dfc76a85a1ab9d857c7d90db0e1110fb314a
Marcarrian <marc.muster@bluewin.ch>
2022-11-21 12:49:50 +0100

    deps(solace-message-client): migrate solclientjs to version 10.11.0

--- projects/solace-message-client/src/public-api.ts
+++ projects/solace-message-client/src/public-api.ts
@@ -3,4 +3,3 @@ export {SolaceMessageClientConfig} from './lib/solace-message-client.config';
 export {OAuthAccessTokenProvider} from './lib/oauth-access-token-provider';
 export {SolaceSessionProvider} from './lib/solace-session-provider';
 export {MessageEnvelope, Params, SolaceMessageClient, mapToText, mapToBinary, ObserveOptions, ConsumeOptions, BrowseOptions, PublishOptions, NullSolaceMessageClient, Data, RequestOptions} from './lib/solace-message-client';
-export * from './lib/solclientjs-typedef-augmentation';
