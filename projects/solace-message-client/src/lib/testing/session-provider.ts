86e9b82555c0ca45039fb17c1f0ed3c9bf4a9353
danielwiehl <daniel.wiehl@yahoo.com>
2024-05-13 14:44:37 +0200

    test(solace-message-client): refactor tests as prerequisite to split tests into separate files
    The large number of tests in a single file makes it difficult to maintain and add new tests. Initializing the session in 'beforeEach' blocks makes it difficult to get an overview and prevents different setups. As a first step, the fixtures were prepared for reuse and moved to separate files.

---
+++ projects/solace-message-client/src/lib/testing/session-provider.ts
@@ -0,0 +1,19 @@
+import {EnvironmentProviders, makeEnvironmentProviders} from '@angular/core';
+import {SolaceSessionProvider} from '../solace-session-provider';
+import {SessionFixture} from './session.fixture';
+import {SessionProperties} from 'solclientjs';
+
+/**
+ * Creates {@link EnvironmentProviders} with a {@link SolaceSessionProvider} that provides the {@link Session} of the passed {@link SessionFixture}.
+ */
+export function provideSession(sessionFixture: SessionFixture): EnvironmentProviders {
+  const sessionProvider: jasmine.SpyObj<SolaceSessionProvider> = jasmine.createSpyObj('SolaceSessionProvider', ['provide']);
+  sessionProvider.provide.and.callFake((properties: SessionProperties) => {
+    sessionFixture.sessionProvider.provide(properties);
+    return sessionFixture.session;
+  });
+
+  return makeEnvironmentProviders([
+    {provide: SolaceSessionProvider, useValue: sessionProvider},
+  ]);
+}
