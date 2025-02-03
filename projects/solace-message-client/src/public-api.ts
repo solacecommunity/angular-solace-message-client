e29d37ae982ded7720753e6ee2357bcf86f47616
k-genov <konstantin.genov@gmail.com>
2025-02-03 16:58:59 +0100

    chore: use Angular 19 tsconfig defaults

--- projects/solace-message-client/src/public-api.ts
+++ projects/solace-message-client/src/public-api.ts
@@ -1,5 +1,5 @@
-export {SolaceMessageClientConfig, SolaceMessageClientConfigFn} from './lib/solace-message-client.config';
-export {OAuthAccessTokenFn} from './lib/oauth-access-token-provider';
+export {type SolaceMessageClientConfig, type SolaceMessageClientConfigFn} from './lib/solace-message-client.config';
+export {type OAuthAccessTokenFn} from './lib/oauth-access-token-provider';
 export {SolaceSessionProvider} from './lib/solace-session-provider';
 export {provideSolaceMessageClient, provideNullSolaceMessageClient} from './lib/solace-message-client.provider';
-export {MessageEnvelope, Params, SolaceMessageClient, mapToText, mapToBinary, ObserveOptions, ConsumeOptions, PublishOptions, NullSolaceMessageClient, Data, RequestOptions} from './lib/solace-message-client';
+export {type MessageEnvelope, type Params, SolaceMessageClient, mapToText, mapToBinary, type ObserveOptions, type ConsumeOptions, type PublishOptions, NullSolaceMessageClient, type Data, type RequestOptions} from './lib/solace-message-client';
