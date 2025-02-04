cff1ad2cd5f339a2884c2563bcd9579978b195b9
k-genov <konstantin.genov@gmail.com>
2025-02-04 14:16:55 +0100

    chore(tryme): remove usages of the deprecated `connect` and `disconnect` methods of `SolaceMessageClient`

--- apps/solace-message-client-testing-app/src/app/app.routes.ts
+++ apps/solace-message-client-testing-app/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {LoginComponent} from './login/login.component';
 import {TryMeComponent} from './try-me/try-me.component';
 import {SessionConfigStore} from './session-config-store';
 import {inject} from '@angular/core';
+import {provideSolaceMessageClient} from '@solace-community/angular-solace-message-client';
 
 export const routes: Routes = [
   {
     path: '',
     canActivate: [requireLogin],
     component: TryMeComponent,
+    providers: [provideSolaceMessageClient(SessionConfigStore.load())],
   },
   {
     path: 'login',
